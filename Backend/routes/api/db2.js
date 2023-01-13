270836095c992c2c9e8abd8107984eccc460b691
John Macadam <jackmac@ibm.com>
2023-01-13 11:02:37 -0600

    changed frontend to react and added in backend. moved files  from new version of beelines into this one

---
+++ Backend/routes/api/db2.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const ibm_db = require('ibm_db')
+
+//DB2 connection details
+const host = process.env.db2_host;
+const port = process.env.db2_PORT;
+const username = process.env.db2_use;
+const password = process.env.db2_pass;
+const database = process.env.database_name;
+
+//  connection string
+const connectionString = `DATABASE=${database};HOSTNAME=${host};PORT=${port};PROTOCOL=TCPIP;UID=${username};PWD=${password};Security=SSL;`;
+
+// Connect to the database
+const conn = new ibm_db.Database();
+
+router.get('/', (req, res) => {
+  // Execute a SQL query
+  console.log("executing an SQL query")
+  ibm_db.open(connectionString, (err, conn) => {
+    //conn is already open now if err is falsy
+    const sql = 'SELECT * FROM FLIGHTS';
+    conn.query(sql, (err, data) => {
+      if (err) {
+        
+        console.error('Error executing SQL: ', err);
+        res.sendStatus(500);
+      } else {
+        console.log('Sending SQL data!', data)
+        res.send(data);
+      }
+        
+      
+    });
+    
+  });
+});
+
+//now sending a post request
+router.post('/post', (req, res) => {
+  console.log("executing a POST SQL query")
+  const body = req.body.input
+
+  const sql = `INSERT INTO FLIGHTS VALUES (3, 'Monroe LA', 'Dallas TX')`;
+  ibm_db.open(connectionString, (err, conn) => {
+    
+    conn.query(sql, (err, data) => {
+      if (err) {    
+        console.error('Error executing SQL: ', err);
+        res.sendStatus(500);
+      } else {
+        console.log('Sending SQL data!', data)
+        res.send(data);
+      }  
+    });
+  })
+})
+
+
+router.patch('/update', (req, res) => {
+  console.log('MAking an update request')
+  const sql = `UPDATE FLIGHTS SET DESTINATION = 'Chicago Il' WHERE ID = 1`
+
+  ibm_db.open(connectionString, (err, conn) => {
+    
+    conn.query(sql, (err, data) => {
+      if (err) {    
+        console.error('Error executing SQL: ', err);
+        res.sendStatus(500);
+      } else {
+        console.log('Sending SQL data!', data)
+        res.send(data);
+      }  
+    });
+  })
+})
+
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,28 @@
 // 1. Import Depedencies
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
+
 
 // 1.1 Allow parsing on request bodies
 app.use(express.json())
+app.use(cors())
 
 // 2. Import routes for api
 const watsonRoutes = require("./routes/api/watson")
+const db2Routes = require('./routes/api/db2')
 
 // 2.1 Direct Requests to /api/watson Routes
 app.use("/api/watson", watsonRoutes)
+app.use('/api/db2', db2Routes)
+
 
 // 3. Start server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log("Server listening on port ", port);
-    
+    console.log("Server listening on port ", port); 
 })
+
+// app.get("/api/watson/session");
 
